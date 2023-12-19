cbaab0536ce5d899b1145629f3d0669797ef9751
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-12-20 01:33:48 +0530

    lab 5 completed

--- Labworks/LabEx-5/searchSort.c
+++ Labworks/LabEx-5/searchSort.c
@@ -3,6 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 
+int comparisons = 0;
+int dataTransfers = 0;
+
+void resetMetrics()
+{
+    comparisons = 0;
+    dataTransfers = 0;
+}
+
+void printMetrics(const char *operation)
+{
+    printf("\nMetrics for %s:\n", operation);
+    printf("Total Element Comparisons: %d\n", comparisons);
+    printf("Total Data Transfer Operations: %d\n", dataTransfers);
+}
+
+
 void printArray(int arr[], int n)
 {
 	int i;
@@ -14,9 +31,10 @@ void printArray(int arr[], int n)
 
 void numBinarySearch()
 {
+	resetMetrics();
 	int arr[] = {2,4,6,8,9,0,10,13,20,12};
 	int n = sizeof(arr) / sizeof(arr[0]); // Number of elements in array
-	int x = 8;							  // Element to be searched for
+	int x = 20;							  // Element to be searched for
 	int low = 0;						  // Starting index
 	int high = n - 1;					  // Ending index
 	printf("\nGiven array is\n");
@@ -43,9 +61,11 @@ void numBinarySearch()
 	while (low <= high)
 	{
 		int mid = (high + low) / 2;
+		comparisons++; // Increment comparison count
 		if (x == arr[mid])
 		{
 			printf("\n\nElement found at index %d\n", mid);
+			printMetrics("Binary Search");
 			found=1;
 			return;
 		}
@@ -62,11 +82,13 @@ void numBinarySearch()
 	if(!found){
 		printf("\n\nElement not found.\n");
 	}
+	
 }
 
 void insertionSort()
 {
 	// perform insertion sort on numbers
+	resetMetrics();
 	int n = 5;
 	int arr[10] = {3, 7, 2, 1, 9,50,32,12,4,6};
 	printf("Insertion Sort:\n");
@@ -78,20 +100,26 @@ void insertionSort()
 		int j = i - 1;
 		while (!(j < 0 || key < arr[j]))
 		{
-			arr[j + 1] = arr[j];
+			comparisons++; // Increment comparison count
+            arr[j + 1] = arr[j];
+            dataTransfers++; // Increment data transfer count
+
 			j -= 1;
 		}
 		arr[j + 1] = key;
 	}
 	printf("\nSorted array is: \n");
 	printArray(arr, n);
+	printMetrics("Insertion Sort ");
+
 }
 
 void bubbleSort()
 {
+	resetMetrics();
 	// perform bubble sort
-	int n = 5;
-	int arr[5] = {64, 25, 12, 22, 11,45,0,14,55,90};
+	int n = 10;
+	int arr[10] = {64, 25, 12, 22, 11,45,0,14,55,90};
 	printf("\nBubble Sort : ");
 	printf("\nGiven array is\n");
 	printArray(arr, n);
@@ -101,8 +129,10 @@ void bubbleSort()
 	{
 		for (int j = 0; j < n - i - 1; j++)
 		{
+			comparisons++; // Increment comparison count
 			if (arr[j] > arr[j + 1])
 			{
+				dataTransfers += 3; // Increment data transfer count for three swaps
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
@@ -111,10 +141,13 @@ void bubbleSort()
 	}
     printf("\nSorted array is: \n");
 	printArray(arr, n);
+	printMetrics("Bubble Sort ");
+
 }
 
 void numSentinelSearch()
 {
+	resetMetrics();
 	// perform sentinel search on numbers
 	int x = 8;
 	int arr[] = {-2, 10, 23, 45, 66, 89};
@@ -132,6 +165,7 @@ void numSentinelSearch()
 	// Search for the target value
 	for (int i = 0; i < n; ++i)
 	{
+		comparisons++;
 		if (arr[i] == x)
 		{
 			found = 1;
@@ -142,6 +176,7 @@ void numSentinelSearch()
 	if (found)
 	{
 		printf("Element %d found at position %d.\n", x, position);
+		printMetrics("Sentinel search (Numbers)");
 	}
 	else
 	{
@@ -151,6 +186,7 @@ void numSentinelSearch()
 
 void stringSentinelSearch()
 {
+	resetMetrics();
 	// perform sentinel search on String
 	char str[] = "The cute cow ate the green grass";
 	char ch = 'g';
@@ -166,6 +202,7 @@ void stringSentinelSearch()
 	// Search for the target character
 	for (int i = 0; i < len; ++i)
 	{
+		comparisons++;
 		if (str[i] == ch)
 		{
 			count++;
@@ -185,11 +222,13 @@ void stringSentinelSearch()
 		{
 			printf("First occurrence of '%c' is at position %d.\n", ch, position);
 		}
+		printMetrics("Sentinel Search (Character)");
 	}
 }
 
 void numLinearSearch()
 {
+	resetMetrics();
 	// perform linear search
 	int x = 10;
 	int arr[] = {-2, 10, 22, 35, 42, 50, 7};
@@ -201,6 +240,7 @@ void numLinearSearch()
 	int foundAt = -1;
 	for (int i = 0; i < size; ++i)
 	{
+		comparisons++;
 		if (x == arr[i])
 		{
 			foundAt = i;
@@ -208,15 +248,21 @@ void numLinearSearch()
 		}
 	}
 	if (foundAt != -1)
+		{
 		printf("Element is present at index %d\n", foundAt);
+		printMetrics("Linear Search (Numbers)");
+		}
+
 	else
 	{
 		printf("Element is not present in array\n");
+		
 	}
 }
 
 void stringLinearSearch()
 {
+	resetMetrics();
 	// perform a linear search or a string character by character
 	char str[] = "The cute cow ate the green grass";
 	char ch = 'g';
@@ -225,6 +271,7 @@ void stringLinearSearch()
 	int flag = -1;
 	for (int i = 0; i < len; i++)
 	{
+		comparisons++;
 		if (str[i] == ch)
 		{
 			flag = i;
@@ -232,7 +279,8 @@ void stringLinearSearch()
 		}
 	}
 	if (flag != -1)
-		printf("Character First Found at position %d\n", flag);
+		{printf("Character First Found at position %d\n", flag);
+		printMetrics("Linear Search (String)");}
 	else
 	{
 		printf("Character Not Found\n");
